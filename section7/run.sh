124d346bff0778f206dcd05074e6109631cbb318
Yuto Watanabe <yuto.w51942@gmail.com>
2021-05-24 20:05:41 +0900

    修正

--- section7/run.sh
+++ section7/run.sh
@@ -1,7 +1,8 @@
 if [ $1 ]; then
   dir_path=$(cd $(dirname $0); pwd)
-  export CLASSPATH=$dir_path/libs/postgresql-42.2.20.jar:.
-  exec `java $1`
+  export CLASSPATH=.:$dir_path/libs/postgresql-42.2.20.jar
+  java $1
+  echo $a
 else
   echo "Please args"
 fi
